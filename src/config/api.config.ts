bd654389817899fdf76aeafd7c7db942bd860676
Ricardo <ricbapdevs@gmail.com>
2021-02-02 17:11:37 -0300

    Criando Interceptor para tratamento de erros

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
     baseUrl: "http://localhost:8080",
-    bucketBaseUrl: "https://bazar-udemy.s3.amazonaws.com/cp1.jpg"
+    bucketBaseUrl: "https://bazar-udemy.s3.amazonaws.com"
 }
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -33,7 +33,7 @@ export class CategoriasPage {
         this.itens = response
       },
       error => {
-        console.log(error)
+       // console.log(error)
       })
 
   }
