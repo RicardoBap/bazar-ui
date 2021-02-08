e28e9b6b9b0453a134f78fafb23250921b116e15
Ricardo <ricbapdevs@gmail.com>
2021-02-08 10:32:54 -0300

    Tela de escolha de Pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -55,7 +55,8 @@ export class PickAddressPage {
 
   nextPage(item: EnderecoDTO) {
     this.pedido.enderecoEntrega = { id: item.id }
-    console.log(this.pedido)
+    //console.log(this.pedido)
+    this.navCtrl.push('PaymentPage', { pedido: this.pedido })
   }
 
 }
