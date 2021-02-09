70a110cb05654b47fc983880a1a6dcb3f209297c
Ricardo <ricbapdevs@gmail.com>
2021-02-09 08:28:39 -0300

    Tela de confirmacao de Pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -21,6 +21,11 @@ export class ClienteService {
             `${API_CONFIG.baseUrl}/clientes/email?value=${email}`)  //{ headers: authHeader }
     }
 
+    findById(id: string) {
+        return this.http.get(
+            `${API_CONFIG.baseUrl}/clientes/${id}`) 
+    }
+
     getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
         return this.http.get(url, { responseType: 'blob' })
